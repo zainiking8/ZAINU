9fa704e4cda7e82bfee39f1822ee1b5b994d0490
Dev093235 <dev431140@gmail.com>
2025-06-15 00:18:42 +0530

    Update pending.js

--- Priyansh/commands/pending.js
+++ Priyansh/commands/pending.js
@@ -22,7 +22,7 @@ module.exports.languages = {
     "en": {
         "invaildNumber": "%1 is not an invalid number",
         "cancelSuccess": "Refused %1 thread!",
-        "notiBox": "Priyansh BoT Connected Successfully!\nUse +help for more info :>",
+        "notiBox": "🆁︎🆄︎🅳︎🆁︎🅰︎ BoT Connected Successfully!\nUse +help2 for more info :>",
         "approveSuccess": "Approved successfully %1 threads!",
 
         "cantGetPendingList": "Can't get the pending list!",
