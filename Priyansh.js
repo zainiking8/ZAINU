e2b274cd7abc87e9ddaef43be8b2b73992c12b09
Dev093235 <dev431140@gmail.com>
2025-07-02 12:17:25 +0530

    Update Priyansh.js

--- Priyansh.js
+++ Priyansh.js
@@ -1,29 +1,175 @@
-const moment = require("moment-timezone"); const { readdirSync, readFileSync, writeFileSync, existsSync, unlinkSync, rm } = require("fs-extra"); const { join, resolve } = require("path"); const { execSync } = require('child_process'); const logger = require("./utils/log.js"); const login = require("fca-smart-shankar"); const axios = require("axios"); const listPackage = JSON.parse(readFileSync('./package.json')).dependencies; const listbuiltinModules = require("module").builtinModules;
+const moment = require("moment-timezone");
+const { readdirSync, readFileSync, writeFileSync, existsSync, unlinkSync } = require("fs-extra");
+const { join, resolve } = require("path");
+const { execSync } = require('child_process');
+const logger = require("./utils/log.js");
+const login = require("fca-smart-shankar");
+const axios = require("axios");
+const listPackage = JSON.parse(readFileSync('./package.json')).dependencies;
+const listbuiltinModules = require("module").builtinModules;
 
-// ... (rest of the code remains unchanged until getText references)
+global.client = {
+  commands: new Map(),
+  events: new Map(),
+  cooldowns: new Map(),
+  eventRegistered: [],
+  handleSchedule: [],
+  handleReaction: [],
+  handleReply: [],
+  mainPath: process.cwd(),
+  configPath: "",
+  getTime: function (option) {
+    return moment.tz("Asia/Kolkata").format({
+      seconds: "ss",
+      minutes: "mm",
+      hours: "HH",
+      date: "DD",
+      month: "MM",
+      year: "YYYY",
+      fullHour: "HH:mm:ss",
+      fullYear: "DD/MM/YYYY",
+      fullTime: "HH:mm:ss DD/MM/YYYY"
+    }[option]);
+  }
+};
 
-try { var appStateFile = resolve(join(global.client.mainPath, global.config.APPSTATEPATH || "appstate.json")); var appState = require(appStateFile); logger.loader(global.getText("rudra", "foundPathAppstate")) } catch { return logger.loader(global.getText("rudra", "notFoundPathAppstate"), "error") }
+global.data = {
+  threadInfo: new Map(),
+  threadData: new Map(),
+  userName: new Map(),
+  userBanned: new Map(),
+  threadBanned: new Map(),
+  commandBanned: new Map(),
+  threadAllowNSFW: [],
+  allUserID: [],
+  allCurrenciesID: [],
+  allThreadID: []
+};
 
-//========= Login account and start Listen Event =========//
+global.utils = require("./utils");
+global.nodemodule = {};
+global.config = {};
+global.configModule = {};
+global.moduleData = [];
+global.language = {};
 
-function onBot({ models: botModel }) { const loginData = {}; loginData['appState'] = appState; login(loginData, async(loginError, loginApiData) => { if (loginError) return logger(JSON.stringify(loginError), ERROR); loginApiData.setOptions(global.config.FCAOption) writeFileSync(appStateFile, JSON.stringify(loginApiData.getAppState(), null, '\x09')) global.client.api = loginApiData global.config.version = '1.2.14' global.client.timeStart = new Date().getTime(), function () { const listCommand = readdirSync(global.client.mainPath + '/Priyansh/commands').filter(command => command.endsWith('.js') && !command.includes('example') && !global.config.commandDisabled.includes(command)); for (const command of listCommand) { try { var module = require(global.client.mainPath + '/Priyansh/commands/' + command); if (!module.config || !module.run || !module.config.commandCategory) throw new Error(global.getText('rudra', 'errorFormat')); if (global.client.commands.has(module.config.name || '')) throw new Error(global.getText('rudra', 'nameExist')); if (!module.languages || typeof module.languages != 'object' || Object.keys(module.languages).length == 0) logger.loader(global.getText('rudra', 'notFoundLanguage', module.config.name), 'warn'); if (module.config.dependencies && typeof module.config.dependencies == 'object') { for (const reqDependencies in module.config.dependencies) { const reqDependenciesPath = join(__dirname, 'nodemodules', 'node_modules', reqDependencies); try { if (!global.nodemodule.hasOwnProperty(reqDependencies)) { if (listPackage.hasOwnProperty(reqDependencies) || listbuiltinModules.includes(reqDependencies)) global.nodemodule[reqDependencies] = require(reqDependencies); else global.nodemodule[reqDependencies] = require(reqDependenciesPath); } else ''; } catch { var check = false; var isError; logger.loader(global.getText('rudra', 'notFoundPackage', reqDependencies, module.config.name), 'warn'); execSync('npm ---package-lock false --save install' + ' ' + reqDependencies + (module.config.dependencies[reqDependencies] == '' || module.config.dependencies[reqDependencies] == '' ? '' : '@' + module.config.dependencies[reqDependencies]), { 'stdio': 'inherit', 'env': process['env'], 'shell': true, 'cwd': join(__dirname, 'nodemodules') }); for (let i = 1; i <= 3; i++) { try { require['cache'] = {}; if (listPackage.hasOwnProperty(reqDependencies) || listbuiltinModules.includes(reqDependencies)) global['nodemodule'][reqDependencies] = require(reqDependencies); else global['nodemodule'][reqDependencies] = require(reqDependenciesPath); check = true; break; } catch (error) { isError = error; } if (check || !isError) break; } if (!check || isError) throw global.getText('rudra', 'cantInstallPackage', reqDependencies, module.config.name, isError); } } logger.loader(global.getText('rudra', 'loadedPackage', module.config.name)); } if (module.config.envConfig) try { for (const envConfig in module.config.envConfig) { if (typeof global.configModule[module.config.name] == 'undefined') global.configModule[module.config.name] = {}; if (typeof global.config[module.config.name] == 'undefined') global.config[module.config.name] = {}; if (typeof global.config[module.config.name][envConfig] !== 'undefined') global['configModule'][module.config.name][envConfig] = global.config[module.config.name][envConfig]; else global.configModule[module.config.name][envConfig] = module.config.envConfig[envConfig] || ''; if (typeof global.config[module.config.name][envConfig] == 'undefined') global.config[module.config.name][envConfig] = module.config.envConfig[envConfig] || ''; } logger.loader(global.getText('rudra', 'loadedConfig', module.config.name)); } catch (error) { throw new Error(global.getText('rudra', 'loadedConfig', module.config.name, JSON.stringify(error))); } if (module.onLoad) { try { const moduleData = {}; moduleData.api = loginApiData; moduleData.models = botModel; module.onLoad(moduleData); } catch (_0x20fd5f) { throw new Error(global.getText('rudra', 'cantOnload', module.config.name, JSON.stringify(_0x20fd5f)), 'error'); }; } if (module.handleEvent) global.client.eventRegistered.push(module.config.name); global.client.commands.set(module.config.name, module); logger.loader(global.getText('rudra', 'successLoadModule', module.config.name)); } catch (error) { logger.loader(global.getText('rudra', 'failLoadModule', module.config.name, error), 'error'); }; } }(), function() { const events = readdirSync(global.client.mainPath + '/Priyansh/events').filter(event => event.endsWith('.js') && !global.config.eventDisabled.includes(event)); for (const ev of events) { try { var event = require(global.client.mainPath + '/Priyansh/events/' + ev); if (!event.config || !event.run) throw new Error(global.getText('rudra', 'errorFormat')); if (global.client.events.has(event.config.name) || '') throw new Error(global.getText('rudra', 'nameExist')); if (event.config.dependencies && typeof event.config.dependencies == 'object') { for (const dependency in event.config.dependencies) { const _0x21abed = join(__dirname, 'nodemodules', 'node_modules', dependency); try { if (!global.nodemodule.hasOwnProperty(dependency)) { if (listPackage.hasOwnProperty(dependency) || listbuiltinModules.includes(dependency)) global.nodemodule[dependency] = require(dependency); else global.nodemodule[dependency] = require(_0x21abed); } else ''; } catch { let check = false; let isError; logger.loader(global.getText('rudra', 'notFoundPackage', dependency, event.config.name), 'warn'); execSync('npm --package-lock false --save install' + dependency + (event.config.dependencies[dependency] == '' || event.config.dependencies[dependency] == '' ? '' : '@' + event.config.dependencies[dependency]), { 'stdio': 'inherit', 'env': process['env'], 'shell': true, 'cwd': join(__dirname, 'nodemodules') }); for (let i = 1; i <= 3; i++) { try { require['cache'] = {}; if (global.nodemodule.includes(dependency)) break; if (listPackage.hasOwnProperty(dependency) || listbuiltinModules.includes(dependency)) global.nodemodule[dependency] = require(dependency); else global.nodemodule[dependency] = require(_0x21abed); check = true; break; } catch (error) { isError = error; } if (check || !isError) break; } if (!check || isError) throw global.getText('rudra', 'cantInstallPackage', dependency, event.config.name); } } logger.loader(global.getText('rudra', 'loadedPackage', event.config.name)); } if (event.config.envConfig) try { for (const _0x5beea0 in event.config.envConfig) { if (typeof global.configModule[event.config.name] == 'undefined') global.configModule[event.config.name] = {}; if (typeof global.config[event.config.name] == 'undefined') global.config[event.config.name] = {}; if (typeof global.config[event.config.name][_0x5beea0] !== 'undefined') global.configModule[event.config.name][_0x5beea0] = global.config[event.config.name][_0x5beea0]; else global.configModule[event.config.name][_0x5beea0] = event.config.envConfig[_0x5beea0] || ''; if (typeof global.config[event.config.name][_0x5beea0] == 'undefined') global.config[event.config.name][_0x5beea0] = event.config.envConfig[_0x5beea0] || ''; } logger.loader(global.getText('rudra', 'loadedConfig', event.config.name)); } catch (error) { throw new Error(global.getText('rudra', 'loadedConfig', event.config.name, JSON.stringify(error))); } if (event.onLoad) try { const eventData = {}; eventData.api = loginApiData, eventData.models = botModel; event.onLoad(eventData); } catch (error) { throw new Error(global.getText('rudra', 'cantOnload', event.config.name, JSON.stringify(error)), 'error'); } global.client.events.set(event.config.name, event); logger.loader(global.getText('rudra', 'successLoadModule', event.config.name)); } catch (error) { logger.loader(global.getText('rudra', 'failLoadModule', event.config.name, error), 'error'); } } }() logger.loader(global.getText('rudra', 'finishLoadModule', global.client.commands.size, global.client.events.size)) logger.loader(Startup Time: ${((Date.now() - global.client.timeStart) / 1000).toFixed()}s)
-logger.loader('===== [ ' + (Date.now() - global.client.timeStart) + 'ms ] =====') writeFileSync(global.client['configPath'], JSON['stringify'](global.config, null, 4), 'utf8') unlinkSync(global['client']['configPath'] + '.temp');
-const listenerData = {}; listenerData.api = loginApiData; listenerData.models = botModel; const listener = require('./includes/listen')(listenerData);
-
-function listenerCallback(error, message) {
-        if (error) return logger(global.getText('rudra', 'handleListenError', JSON.stringify(error)), 'error');
-        if (["presence", "typ", "read_receipt"].some(data => data == message.type)) return;
-        if (global.config.DeveloperMode == !![]) console.log(message);
-        return listener(message);
-    };
-    global.handleListen = loginApiData.listenMqtt(listenerCallback);
-    try {
-        await checkBan(loginApiData);
-    } catch (error) {
-        return;
-    };
-    if (!global.checkBan) logger(global.getText('rudra', 'warningSourceCode'), '[ GLOBAL BAN ]');
-});
-
+let configValue;
+try {
+  global.client.configPath = join(global.client.mainPath, "config.json");
+  configValue = require(global.client.configPath);
+  logger.loader("Found file config: config.json");
+} catch {
+  const tempPath = global.client.configPath.replace(/\.json$/, ".temp");
+  if (existsSync(tempPath)) {
+    configValue = JSON.parse(readFileSync(tempPath));
+    logger.loader(`Found: ${tempPath}`);
+  } else return logger.loader("config.json not found!", "error");
 }
 
+try {
+  Object.assign(global.config, configValue);
+  logger.loader("Config Loaded!");
+} catch {
+  return logger.loader("Can't load file config!", "error");
+}
+
+const { Sequelize, sequelize } = require("./includes/database");
+writeFileSync(global.client.configPath + ".temp", JSON.stringify(global.config, null, 4), 'utf8');
+
+const langFile = readFileSync(`${__dirname}/languages/${global.config.language || "en"}.lang`, 'utf-8').split(/\r?\n|\r/);
+for (const line of langFile.filter(l => l && !l.startsWith('#'))) {
+  const [key, ...rest] = line.split('=');
+  const head = key.split('.')[0];
+  const subKey = key.replace(`${head}.`, '');
+  const value = rest.join('=').replace(/\\n/g, '\n');
+  if (!global.language[head]) global.language[head] = {};
+  global.language[head][subKey] = value;
+}
+
+global.getText = function (...args) {
+  const langText = global.language;
+  if (!langText[args[0]]) throw `Language key not found: ${args[0]}`;
+  let text = langText[args[0]][args[1]];
+  for (let i = 2; i < args.length; i++) {
+    text = text.replace(new RegExp(`%${i - 1}`, 'g'), args[i]);
+  }
+  return text;
+};
+
+let appState;
+try {
+  const appStateFile = resolve(join(global.client.mainPath, global.config.APPSTATEPATH || "appstate.json"));
+  appState = require(appStateFile);
+  logger.loader(global.getText("rudra", "foundPathAppstate"));
+} catch {
+  return logger.loader(global.getText("rudra", "notFoundPathAppstate"), "error");
+}
+
+function onBot({ models: botModel }) {
+  const appStateFile = resolve(join(global.client.mainPath, global.config.APPSTATEPATH || "appstate.json"));
+  login({ appState }, async (loginError, loginApiData) => {
+    if (loginError) return logger(JSON.stringify(loginError), "ERROR");
+
+    loginApiData.setOptions(global.config.FCAOption);
+    writeFileSync(appStateFile, JSON.stringify(loginApiData.getAppState(), null, '\t'));
+    global.client.api = loginApiData;
+    global.config.version = '1.2.14';
+    global.client.timeStart = Date.now();
+
+    (() => {
+      const listCommand = readdirSync(join(global.client.mainPath, '/Rudra/commands'))
+        .filter(f => f.endsWith('.js') && !f.includes('example') && !global.config.commandDisabled.includes(f));
+
+      for (const file of listCommand) {
+        try {
+          const command = require(join(global.client.mainPath, '/Rudra/commands', file));
+          if (!command.config || !command.run || !command.config.commandCategory) throw new Error(global.getText('rudra', 'errorFormat'));
+          global.client.commands.set(command.config.name, command);
+          logger.loader(global.getText('rudra', 'successLoadModule', command.config.name));
+        } catch (err) {
+          logger.loader(global.getText('rudra', 'failLoadModule', file, err.message), 'error');
+        }
+      }
+    })();
+
+    (() => {
+      const events = readdirSync(join(global.client.mainPath, '/Rudra/events'))
+        .filter(e => e.endsWith('.js') && !global.config.eventDisabled.includes(e));
+
+      for (const file of events) {
+        try {
+          const event = require(join(global.client.mainPath, '/Rudra/events', file));
+          if (!event.config || !event.run) throw new Error(global.getText('rudra', 'errorFormat'));
+          global.client.events.set(event.config.name, event);
+          logger.loader(global.getText('rudra', 'successLoadModule', event.config.name));
+        } catch (err) {
+          logger.loader(global.getText('rudra', 'failLoadModule', file, err.message), 'error');
+        }
+      }
+    })();
+
+    logger.loader(global.getText('rudra', 'finishLoadModule', global.client.commands.size, global.client.events.size));
+    logger.loader(`Startup Time: ${((Date.now() - global.client.timeStart) / 1000).toFixed()}s`);
+    writeFileSync(global.client.configPath, JSON.stringify(global.config, null, 4), 'utf8');
+    unlinkSync(global.client.configPath + ".temp");
+
+    const listener = require('./includes/listen')({ api: loginApiData, models: botModel });
+    global.handleListen = loginApiData.listenMqtt((err, message) => {
+      if (err) return logger(global.getText('rudra', 'handleListenError', JSON.stringify(err)), 'error');
+      if (!['presence', 'typ', 'read_receipt'].includes(message.type)) return listener(message);
+    });
+  });
+}
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    const models = require('./includes/database/model')({ Sequelize, sequelize });
+    logger(global.getText('rudra', 'successConnectDatabase'), '[ DATABASE ]');
+    onBot({ models });
+  } catch (error) {
+    logger(global.getText('rudra', 'failConnectDatabase', error.message), '[ DATABASE ]');
+  }
+})();
+
+process.on('unhandledRejection', (err, p) => {});
