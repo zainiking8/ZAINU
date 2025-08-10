518fe50477fffe9178c070462e976b9a1842814d
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:14:47 +0500

    Update 0out.js

--- Rudra/commands/0out.js
+++ Rudra/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "ZAINU",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
