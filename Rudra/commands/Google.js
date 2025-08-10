1764e60a2bfc05890517d5d53d1548368669b7d2
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:16:06 +0500

    Update Google.js

--- Rudra/commands/Google.js
+++ Rudra/commands/Google.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "ZAINU",
 	description: "Comment on table ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "google [text]",
