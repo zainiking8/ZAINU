12d146a61d6bd794062f8e2823390c6ebf3b7ea9
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:10:23 +0500

    Update botban.js

--- Rudra/commands/botban.js
+++ Rudra/commands/botban.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "otherbots",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "ZAINU X BRAND",
   description: "ban otherbot",
   commandCategory: "config",
   cooldowns: 0
