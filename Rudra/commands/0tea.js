5b18af13f242e7481511de7321f73c81385b7628
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:14:01 +0500

    Update 0tea.js

--- Rudra/commands/0tea.js
+++ Rudra/commands/0tea.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "tea",
     version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+  credits: "ZAINU", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "tea",
