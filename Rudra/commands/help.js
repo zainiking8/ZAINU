4b3a1c0550bea1f8feed751f85c44eb0777e5a78
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:03:31 +0500

    Update help.js

--- Rudra/commands/help.js
+++ Rudra/commands/help.js
@@ -2,7 +2,7 @@
 	name: "help",
 	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "ZAIN JOIYA",
 	description: "Beginner's Guide",
 	commandCategory: "system",
 	usages: "[Tên module]",
