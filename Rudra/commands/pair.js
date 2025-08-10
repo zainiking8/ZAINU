e0384a0a4aa68d6774ec9d796b54a971344f9fd6
zainiking8 <101806342+zainiking8@users.noreply.github.com>
2025-08-10 16:21:10 +0500

    Update pair.js

--- Rudra/commands/pair.js
+++ Rudra/commands/pair.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pair",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "ZAINU X BRAND",
   description: "It's a compound :>",
   commandCategory: "Giải trí",
   usages: "",
